4ceae1d172536e3d5d0ee2a9317a65587e531a85
MuhammadLateef <ml129kmg@gmail.com>
2025-11-19 16:40:59 +0500

    remove nav and footer from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,14 @@
-import Navbar from "@/components/Navbar/navbar";
 import { Hero } from "@/components/Hero/index";
 import { WhoAreWeSection } from '@/components/who-are-we/who-are-we-section'
 import { HowItWorksSection } from '@/components/how-it-works/how-it-works-section'
 import { BuyAndSellSection } from '@/components/Buy-and-Sell/by-and-sell-section'
-import  Footer  from '@/components/footer/footer'
 export default function Home() {
   return (
     <main>
-      <Navbar />
       <Hero />
       <WhoAreWeSection />
       <HowItWorksSection />
       <BuyAndSellSection />
-      <Footer />
       
     </main>
   );
